2bd448c64ba752bd8567b14211e34b0d929e2442
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-02-20 21:12:40 +0400

    fix capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,13 @@
 import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hexlet') !== 'Hexlet') {
-  throw new Error('Capitalize function eror');
+  throw new Error('Capitalize Function Error: Performance error');
 }
 if (capitalize('') !== '') {
-  throw new Error('Capitalize function eror');
+  throw new Error('Capitalize Function Error: Empty string');
 }
 if (capitalize(' hexlet') !== 'Hexlet') {
-  throw new Error('Capitalize function eror');
+  throw new Error('Capitalize Function Error: String with whitespace at the beginning');
 }
 
-console.log('Tests passed');
+console.log('Tests Passed');
