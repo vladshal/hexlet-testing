0d9a2f1bd214de56664f7be03df8a7f5f8fa92a1
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-02-21 16:44:54 +0400

    delete wrong test dir

--- tests/capitalize.test.js
+++
@@ -1,8 +0,0 @@
-import { strict as assert } from 'node:assert';
-import capitalize from '../src/capitalize.js';
-
-assert.strictEqual(capitalize(''), '');
-assert.strictEqual(capitalize('hello'), 'Hello');
-
-
-console.log('Tests Passed');
