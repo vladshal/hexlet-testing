0038431fd41c12a286479e584e950768d28fdacf
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-02-21 12:52:39 +0400

    fix capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,8 @@
-import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
+import capitalize from '../src/capitalize.js';
 
-if (capitalize('hexlet') !== 'Hexlet') {
-  throw new Error('Capitalize Function Error: Performance error');
-}
-if (capitalize('') !== '') {
-  throw new Error('Capitalize Function Error: Empty string');
-}
-if (capitalize(' hexlet') !== 'Hexlet') {
-  throw new Error('Capitalize Function Error: String with whitespace at the beginning');
-}
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
+
 
 console.log('Tests Passed');
